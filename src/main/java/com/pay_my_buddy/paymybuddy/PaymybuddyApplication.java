2254a97b9c1a16feac171fa9e71185b477907ad9
Clara Slys <slys.clara89000@gmail.com>
2024-03-20 19:47:33 +0100

    project initialization

---
+++ src/main/java/com/pay_my_buddy/paymybuddy/PaymybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.pay_my_buddy.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pay_my_buddy/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay_my_buddy.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
